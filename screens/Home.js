0bebbfd959a9fddf7fdbd7714c9245d805a03dfd
mingnhat <81601321+mingnhat@users.noreply.github.com>
2022-06-25 20:43:18 +0700

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           uri: "https://scontent.fsgn13-2.fna.fbcdn.net/v/t1.6435-9/151697206_733889480656784_8076122449299495101_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NPRIvdFhAiUAX-Mkrn0&tn=2E6CmETA3lTLyyz1&_nc_ht=scontent.fsgn13-2.fna&oh=00_AT_9WFxDHsC6i0enVy25tF_P3W_NDUzzp-FuuzArg_EWVA&oe=62DD6A51",
         }}
       />
-      <Text style={{ backgroundColor: "black",color:"white", flex: 1, alignItems:'center',justifyContent:'center',alignContent:'center' }}>this is Home screen</Text>
+      <Text style={{ backgroundColor: "black",color:"white", flex: 1, alignItems:'center',justifyContent:'center',alignContent:'center' }}>This is Home screen and MinhNhat photo</Text>
     </View>
   );
 };
